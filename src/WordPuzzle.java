26cb9e55aa78710c72bd6aed1fc6b04d897774da
Nicole Huang <nicoleh87@nycstudents.net>
2022-03-15 12:28:50 -0400

    Initial Commit

--- src/WordPuzzle.java
+++ src/WordPuzzle.java
@@ -1,2 +1,17 @@
 public class WordPuzzle {
+    int time;
+    boolean hard;
+    int words;
+
+    public WordPuzzle(int time, boolean hard, int words)
+    {
+        time = this.time;
+        hard = this.hard;
+        words = this.words;
+    }
+
+    public void guess()
+    {
+        System.out.println("Your guess has been entered");
+    }
 }
